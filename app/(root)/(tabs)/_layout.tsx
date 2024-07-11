1830811a6fd2663a7c37abf24722d7a2c348f1b0
yuefii <muffidahmadiawan31@gmail.com>
2024-07-11 08:05:20 +0700

    feat: features(navigation) ui

--- app/(root)/(tabs)/_layout.tsx
+++ app/(root)/(tabs)/_layout.tsx
@@ -1,17 +1,71 @@
 import React from 'react';
-import FontAwesome from '@expo/vector-icons/FontAwesome';
 import Colors from '@/constants/Colors';
+import FontAwesome from '@expo/vector-icons/FontAwesome';
 
 import { Link, Tabs } from 'expo-router';
-import { Pressable } from 'react-native';
+import { MaterialIcons } from '@expo/vector-icons';
 import { useColorScheme } from '@/hooks/useColorScheme';
 import { useClientOnlyValue } from '@/hooks/useClientOnlyValue';
+import { Pressable, StyleProp, TextStyle } from 'react-native';
+import Animated, { useAnimatedStyle, useSharedValue, withSpring } from 'react-native-reanimated';
 
-function TabBarIcon(props: {
-  name: React.ComponentProps<typeof FontAwesome>['name'];
+type IconLibrary = 'FontAwesome' | 'MaterialIcons';
+interface TabBarIconProps {
+  name: React.ComponentProps<typeof FontAwesome>['name'] | React.ComponentProps<typeof MaterialIcons>['name'];
   color: string;
-}) {
-  return <FontAwesome size={28} style={{ marginBottom: -3 }} {...props} />;
+  library: IconLibrary;
+  style?: StyleProp<TextStyle>;
+  focused?: boolean;
+}
+
+const AnimatedFontAwesome = Animated.createAnimatedComponent(FontAwesome);
+const AnimatedMaterialIcons = Animated.createAnimatedComponent(MaterialIcons);
+
+function TabBarIcon({
+  name,
+  color,
+  library,
+  style,
+  focused
+}: TabBarIconProps) {
+  const scale = useSharedValue(1);
+
+  const animatedStyle = useAnimatedStyle(() => ({
+    transform: [{ scale: withSpring(scale.value) }],
+  }));
+
+  React.useEffect(() => {
+    scale.value = focused ? 1.2 : 1;
+  }, [focused]);
+
+  const iconStyle = [
+    { bottom: 20 },
+    focused && {
+      backgroundColor: "#10b981",
+      paddingHorizontal: 16,
+      paddingVertical: 12,
+      borderRadius: 30,
+      height: 50
+    },
+    style,
+    animatedStyle
+  ];
+
+  if (library === 'FontAwesome') {
+    return <AnimatedFontAwesome
+      name={name as React.ComponentProps<typeof FontAwesome>['name']}
+      size={30}
+      color={color}
+      style={iconStyle} />;
+  } else if (library === 'MaterialIcons') {
+    return <AnimatedMaterialIcons
+      name={name as React.ComponentProps<typeof MaterialIcons>['name']}
+      size={30}
+      color={color}
+      style={iconStyle} />;
+  } else {
+    return null;
+  }
 }
 
 export default function TabLayout() {
@@ -22,12 +76,35 @@ export default function TabLayout() {
       screenOptions={{
         tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
         headerShown: useClientOnlyValue(false, true),
+        tabBarStyle: {
+          backgroundColor: 'black',
+          borderTopWidth: 0,
+          padding: 0
+        },
+        tabBarShowLabel: false
       }}>
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: 'Profile',
+          tabBarIcon: ({ color, focused }) =>
+            <TabBarIcon
+              name="user"
+              library="FontAwesome"
+              color={color}
+              focused={focused} />,
+        }}
+      />
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Tab One',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+          title: 'Home',
+          tabBarIcon: ({ color, focused }) =>
+            <TabBarIcon
+              name="home"
+              library="FontAwesome"
+              color={color}
+              focused={focused} />,
           headerRight: () => (
             <Link href="/modal" asChild>
               <Pressable>
@@ -45,10 +122,15 @@ export default function TabLayout() {
         }}
       />
       <Tabs.Screen
-        name="two"
+        name="leaderboard"
         options={{
-          title: 'Tab Two',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+          title: 'Leaderboard',
+          tabBarIcon: ({ color, focused }) =>
+            <TabBarIcon
+              name="leaderboard"
+              library="MaterialIcons"
+              color={color}
+              focused={focused} />,
         }}
       />
     </Tabs>
