209d6dfcedf8dc9ca6500a911305f4c27e4f108f
Muhamad Mupid Ahmadiawan <133787157+Yuefii@users.noreply.github.com>
2024-08-08 09:22:56 +0700

    refactor: add register services

--- app/(auth)/register.tsx
+++ app/(auth)/register.tsx
@@ -2,8 +2,37 @@ import { Link } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { View, Text, Pressable, TextInput } from "react-native";
+import { useState } from "react";
 
 export default function Register() {
+    const [name, setName] = useState('')
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+
+    const handleRegister = () => {
+        const apiUrl = process.env.EXPO_PUBLIC_API_URL || ""
+        
+        const requestBody = {
+            name: name,
+            username: username,
+            password: password
+        };
+
+        fetch(apiUrl, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(requestBody),
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log('Response:', data)
+        })
+        .catch(error => {
+            console.error('Error:', error)
+        });
+    };
     return (
         <SafeAreaView
             style={{
@@ -34,8 +63,8 @@ export default function Register() {
                     }}
                     placeholder="Nama Lengkap"
                     placeholderTextColor="#003f5c"
-                // value={name}
-                // onChangeText={text => setName(text)}
+                value={name}
+                onChangeText={text => setName(text)}
                 />
             </View>
             <View style={{
@@ -54,8 +83,8 @@ export default function Register() {
                     }}
                     placeholder="Nama Pengguna"
                     placeholderTextColor="#003f5c"
-                // value={username}
-                // onChangeText={text => setUsername(text)}
+                value={username}
+                onChangeText={text => setUsername(text)}
                 />
             </View>
             <View style={{
@@ -75,8 +104,8 @@ export default function Register() {
                     placeholder="Kata Sandi"
                     placeholderTextColor="#003f5c"
                     secureTextEntry={true}
-                // value={password}
-                // onChangeText={text => setPassword(text)}
+                value={password}
+                onChangeText={text => setPassword(text)}
                 />
             </View>
             <Pressable style={{
@@ -87,7 +116,8 @@ export default function Register() {
                 marginVertical: 10,
                 marginHorizontal: 10,
                 alignItems: 'center',
-            }}>
+            }}
+            onPress={handleRegister}>
                 <Text style={{
                     color: 'white',
                     fontSize: 16,
